eeae307dbba1cdfb1fc5de18339868ec6065efee
Mozaick <mozeselbarbary@developersinstitute.co.nz>
2022-02-05 12:58:41 +1300

    Secure signup and validations implemented

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,7 +1,8 @@
 const User = require('../../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const { UserInputError } = require('apollo-server');
+const { validateRegisterInput } = require('../../utils/validators');
 
 module.exports = {
   Mutation: {
@@ -16,6 +17,67 @@ module.exports = {
       { registerInput: { username, email, password, passwordConfirm } },
       context,
       info
-    ) {},
+    ) {
+      // 1) Validate user data
+      const { errors, valid } = validateRegisterInput(
+        username,
+        email,
+        password,
+        passwordConfirm
+      );
+      if (!valid) {
+        // if NOT valid throw the errors obj
+        throw new UserInputError('Validation Errors', { errors });
+      }
+      // 2) Make sure user does not exist
+      const user = await User.findOne({ username });
+      const userEmail = await User.findOne({ email });
+      // return an error if user already exists
+      if (user) {
+        throw new UserInputError('Username already exists!', {
+          // this error obj will be used later in the frontend
+          errors: {
+            username: 'Username is not available!',
+          },
+        });
+      }
+      if (userEmail) {
+        throw new UserInputError('email already exists!', {
+          errors: {
+            email: 'this email is already registered!',
+          },
+        });
+      }
+      // 3) Hash pwd & create auth token
+
+      password = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        username,
+        email,
+        password,
+        passwordConfirm,
+        createdAt: new Date().toISOString(),
+      });
+
+      // save newUser in DB
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+          expiresIn: process.env.JWT_EXPIRES_IN,
+        }
+      );
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
   },
 };

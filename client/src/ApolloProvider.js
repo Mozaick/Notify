85366976204dd6c25e259fa5da3b8105ffc6fd62
Mozaick <mozeselbarbary@developersinstitute.co.nz>
2022-02-14 15:45:19 +1300

    Apollo setup and connecting it to React

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import App from './App';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider,
+} from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"><h1>Hello World!!</h1>
+    <div className="App">
+      <h1>Hello World!!</h1>
     </div>
   );
 }

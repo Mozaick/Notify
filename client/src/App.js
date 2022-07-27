10d9a8dec0cb145805d2222b6855105d419bcafa
Mozes Elbarbary <moz@moz.local>
2022-07-28 11:57:26 +1200

    Creating routes for home, login and register pages && Adding Menuebar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 // import logo from './logo.svg';
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MenuBar from './components/MenuBar';
+import Home from './pages/home';
+import Register from './pages/register';
+import Login from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    <BrowserRouter>
+      <MenuBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
